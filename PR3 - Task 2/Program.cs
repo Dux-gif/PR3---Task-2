4824dadbf19e686769a804f5437448a427fb5257
user <user@DESKTOP-SECB52V>
2024-03-31 13:53:17 +0500

    Добавьте файлы проекта.

---
+++ PR3 - Task 2/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR3___Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Добро пожаловать в игру Blackjack! Сколько у вас карт на руках?");
+            int amountOfCards = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите пожалуйста номинал всех карт");
+            int result = 0;
+
+            for (int i = 0; i < amountOfCards; i++)
+            {
+                if (i == 0)
+                {
+                    Console.Write("Введите номинал первой карты: ");
+                }
+                else if (i < amountOfCards)
+                {
+                    Console.Write("Введите номинал следующей карты: ");
+                }
+                string nominal = Console.ReadLine();
+                switch (nominal)
+                    {
+                        case "2":
+                            result += 2;
+                            break;
+                        case "3":
+                            result += 3;
+                            break;
+                        case "4":
+                            result += 4;
+                            break;
+                        case "5":
+                            result += 5;
+                            break;
+                        case "6":
+                            result += 6;
+                            break;
+                        case "7":
+                            result += 7;
+                            break;
+                        case "8":
+                            result += 8;
+                            break;
+                        case "9":
+                            result += 9;
+                            break;
+                        case "10":
+                            result += 10;
+                            break;
+                        case "J":
+                            result += 10;
+                            break;
+                        case "Q":
+                            result += 10;
+                            break;
+                        case "K":
+                            result += 10;
+                            break;
+                        case "T":
+                            result += 10;
+                            break;
+                    }
+                
+                if (i == amountOfCards)
+                {
+                    break;
+                }
+            }
+            if (result > 21)
+            {
+                Console.WriteLine("Сумма ваших карт: " + result + "\nВы проиграли!");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Сумма ваших карт: " + result);
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ PR3 - Task 2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("PR3 - Task 2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PR3 - Task 2")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("bcaddea3-3dde-4a60-8083-3d3d87e45859")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
